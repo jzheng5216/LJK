694ad8989342120d00cf11a6b978941edfd574a8
Pinyin <kevin101zhang@gmail.com>
2018-10-16 21:53:12 -0400

    Start-Here

--- game.js
+++ game.js
@@ -12,84 +12,92 @@ var toLowerCase = function (check) {
     return check.toLowerCase();
 }// call this function to make everything lowercase
 
-
-$(document).ready(function () {
-    document.onkeyup = function (event) {
-        if (guesses >= 10) {
-            return;
+function updateGuesses(guesses) {
+    if (this.guessesLeft === 0) {
+        this.restartGame();
+    } else {
+        this.updateGuesses(guesses);
+        this.updateMatchedLetters(guesses);
+        this.rebuildWordView();
+        if (this.updateWins() === true) {
+            this.restartGame();
         }
-
-        
-    var letter = event.key;
-        
-    var word = wordList[Math.floor((Math.random() * wordList.length);
-        
-
-
-        var letter = event.key;
-        var word = wordList[Math.floor((Math.random() * wordList.length) + 1)];
-        var answer = word.split(" ");
-
-        
-    
-  updatePage: function (letter) {
-   
-    if (this.guessesLeft === 0) {
-      this.restartGame();
-    }
-    else {
-      this.updateGuesses(guesses);
-      this.updateMatchedLetters(guesses);
-      this.rebuildWordView();
-      if (this.updateWins() === true) {
-        this.restartGame();
-      }
-    }
-updateGuesses: function(guesses) {
-    if (this.guessesLeft === 0) {
-      this.restartGame();
-    }
-    else {
-      this.updateGuesses(guesses);
-      this.updateMatchedLetters(guesses);
-      this.rebuildWordView();
-      if (this.updateWins() === true) {
-        this.restartGame();
-      }
     }
 
-
-   
 }
 
+function updatePage(letter) { //append letter to ___
+
+    if (this.guessesLeft === 0) {
+        this.restartGame();
+    } else {
+        this.updateGuesses(guesses);
+        this.updateMatchedLetters(guesses);
+        this.rebuildWordView();
+        if (this.updateWins() === true) {
+            this.restartGame();
+        }
     }
+
+
 }
-   
-function gameOver () {
-    if (answer.toString() == emptyArray.toString() && guesses <= 10){
+
+function updateMatchedLetters(){
+    console.log("hello, update me");
+}
+
+
+function gameOver() {
+    if (answer.toString() == emptyArray.toString() && guesses <= 10) {
         wins++;
         $("#wins").text(wins);
-    if (answe.toString() !== emptyArray.toString() && guess > 10){
-        loss++;
-        $("#losses").text(losses);
-    }
-
-        
-
-
-
-
-
-
-
-
-
-
+        if (answe.toString() !== emptyArray.toString() && guess > 10) {
+            loss++;
+            $("#losses").text(losses);
+        }
 
 
     }
 
+}
+
+    //type here
+    $(document).ready(function () { // leave this checks if Javascript is ready
+
+        document.onkeyup = function (event) { //captures keystroke(user entry)
+            if (guesses >= 10) {
+                return;//stops at 10
+            }
 
 
-}); 
+            var letter = event.key;
+            var word = wordList[Math.floor((Math.random() * wordList.length) + 1)];
+            var answer = word.split(" ");
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });//end of game over
+
+
+
+
 
