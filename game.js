9c5dd253c88dc5b80b09b90e61873a4b9a3e8391
MaxMagic <Lennox.E.David@gmail.com>
2018-10-16 21:12:42 -0400

    first update

--- game.js
+++ game.js
@@ -1,6 +1,17 @@
-var wordList=["word"];
-var wins
-var loss
-var guesses
-var emptyArray
-var guessingWord
+var wordList=["lion", "zebra", "tiger", "hyena", "panther", "gazelle"];
+var wins = 0;
+var loss = 0;
+var guesses;
+var emptyArray = []
+var guessingWord;
+
+$(document).ready(function () {
+    document.onkeyup = function(event) {
+        var letter = event.key;
+        
+        var word = wordList[Math.floor((Math.random() * 6) + 1)];
+        return word;
+    
+
+}
+   
